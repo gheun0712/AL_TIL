e68eba60181f274becedbb46c8585abb046e941e
Ga Heun Kim <97588019+gheun0712@users.noreply.github.com>
2023-05-05 02:08:07 +0900

    [level 0] Title: 안전지대, Time: 2.57 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(board):
+    dx = [-1, 1, 0, 0, 1, 1, -1, -1]
+    dy = [0, 0, -1, 1, 1, -1, -1, 1]
+    depth = len(board)
+    visited = [[0] * depth for _ in range(depth)]
+    
+    def bfs(x,y):
+        q = deque()
+        q.append((x,y))
+        
+        while q:
+            a, b = q.popleft()
+            visited[a][b] = 1
+            for i in range(8):
+                nx = dx[i] + a
+                ny = dy[i] + b
+                
+                if 0 <= nx < depth and 0 <= ny < depth and visited[nx][ny] != 1:
+                    if board[nx][ny] == 1:
+                        q.append((nx, ny))
+                    else:
+                        board[nx][ny] = 2
+    
+    for i in range(depth):
+        for j in range(depth):
+            if board[i][j] == 1:
+                bfs(i,j)
+    
+    res = 0
+    for i in range(depth):
+        res += board[i].count(0)
+    return res
