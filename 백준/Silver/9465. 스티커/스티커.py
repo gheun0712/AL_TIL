d97fb1aed81c3037d34c36cc2e7380b74d341811
Ga Heun Kim <97588019+gheun0712@users.noreply.github.com>
2023-05-12 03:35:01 +0900

    [Silver I] Title: 스티커, Time: 332 ms, Memory: 170388 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,20 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    lst = []
+    ans = 0
+
+    for _ in range(2):
+        lst.append(list(map(int, input().split())))
+
+    # 위/아래 불가 대각선으로 지그재그 탐색
+    if n > 1:
+        lst[0][1] += lst[1][0]
+        lst[1][1] += lst[0][0]
+
+    for i in range(2, n):
+        lst[0][i] += max(lst[1][i - 1], lst[1][i - 2])
+        lst[1][i] += max(lst[0][i - 1], lst[0][i - 2])
+
+    print(max(lst[0][n - 1], lst[1][n - 1]))
