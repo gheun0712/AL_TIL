7443dc779a56856d30673dceb6e04fe5ea93b8ba
Ga Heun Kim <97588019+gheun0712@users.noreply.github.com>
2023-05-26 23:42:14 +0900

    [unrated] Title: 귤 고르기, Time: 37.07 ms, Memory: 21.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def solution(k, tangerine):
+    answer = 0
+    cnt = sorted(Counter(tangerine).items(), reverse=True, key=lambda x:x[1])
+    
+    for key, value in cnt:
+        if k <= 0:
+            break
+        k -= value
+        answer += 1
+    return answer
